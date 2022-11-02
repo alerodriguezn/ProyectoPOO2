d4ae861d3e3e1e92c5ce202c087aca559d22f8e4
alerodriguezn <navarrocmo@gmail.com>
2022-11-02 16:35:21 -0600

    Registro Login y Clase Usuario

---
+++ ProyectoPOO/src/main/java/com/mycompany/proyectopoo/jugador/Jugador.java
@@ -0,0 +1,75 @@
+package com.mycompany.proyectopoo.jugador;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * Describe al Jugador
+ * @author navar
+ */
+public class Jugador {
+    
+    private String nombre;
+    private String contrasena;
+    private int puntos;
+    
+    public Jugador(String nombre, String contrasena){
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.puntos = 0;// Se inicia el puntaje en 0 para cada usuario
+        
+    }
+
+    /**
+     * Recupera el nombre del jugador
+     * @return nombre del jugador.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+    
+    /**
+     * Recupera los puntos del jugador
+     * @return puntos del jugador.
+     */
+    public int getPuntos() {
+        return puntos;
+    }
+
+    /**
+     * Establece el nombre del jugador
+     * @param nombre nombre del jugador
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Establece los puntos del jugador
+     * @param puntos puntos del jugador
+     */
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+    
+    /**
+     * Agregar puntos al jugador
+     * @param puntos puntos que se van a agregar
+     */
+    public void agregarPuntos(int puntos) {
+        this.puntos += puntos;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+    
+   
+    
+    
+    
+    
+}
